d3e0f865325bdc2d5c5798e4ee32bb82828c85ed
Arek Mytych <amytych@gmail.com>
2016-08-08 08:30:25 +0200

    Ensured that first operand exists
    As a convenience for the user, when choosing the operation before the first
    operand it defaults to 0.

--- Update.elm
+++ Update.elm
@@ -9,6 +9,7 @@ update msg model =
     case msg of
         SetOperation operation ->
             ( { model | operation = operation }
+                |> ensureFirstOperand
                 |> setStatus
             , Cmd.none
             )
@@ -28,6 +29,19 @@ update msg model =
                 |> resetCalc
             , Cmd.none
             )
+
+
+{-| When the user sets operation before setting the first operand
+in a typical calc the operand defaults to 0, it's a convinience for the user
+-}
+ensureFirstOperand : Model -> Model
+ensureFirstOperand model =
+    case ( model.operation, model.operand1 ) of
+        ( Just _, Nothing ) ->
+            { model | operand1 = Just 0 }
+
+        ( _, _ ) ->
+            model
 
 
 setStatus : Model -> Model
